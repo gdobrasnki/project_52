e47561b7fce760d0808f9aa22394ffa059b0b9cd
GJ <GJ@DESKTOP-2Q3EC0I>
2020-03-20 15:05:45 -0600

    Upload init

---
+++ data.py
@@ -0,0 +1,3 @@
+
+FOOD = ['Canned Vegtables', 'Canned Meat', 'Fresh Vegtables', 'Fresh Meat', 'Dried Beans']
+HYGIENE = ['Toilet Paper','ToothPaste','Deodarant','ToothBrush','Shampoo','Soap','HandSoap','Tissues']
